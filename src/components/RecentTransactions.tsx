f206c8fee1349855f49954dd797897e234d33cac
Francis7575 <francis.albert.2009@gmail.com>
2024-08-19 15:17:50 -0700

    added copy, pagination and category components

--- src/components/RecentTransactions.tsx
+++ src/components/RecentTransactions.tsx
@@ -3,6 +3,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BankTabItem } from './BankTabItem'
 import BankInfo from './BankInfo'
 import TransactionsTable from './TransactionsTable'
+import { Pagination } from './Pagination'
 
 const RecentTransactions = ({
   accounts,
@@ -57,6 +58,11 @@ const RecentTransactions = ({
               type="full"
             />
             <TransactionsTable transactions={currentTransactions} />
+            {totalPages > 1 && (
+              <div className="my-4 w-full">
+                <Pagination totalPages={totalPages} page={page} />
+              </div>
+            )}
           </TabsContent>
         ))}
       </Tabs>
